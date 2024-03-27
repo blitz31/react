2e4b3d02a15839171c4d97d9e751580f520bc572
blitzboi <146631108+blitz31@users.noreply.github.com>
2024-03-27 13:00:00 +0000

    tailwind and props

---
+++ tailwind_props/tail_props/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Card from './components/comps'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-blue-400 text-black p-5 rounded-xl mb-4'>Tailwind CSS</h1>
+      <Card userName='Nigga'/>
+    </>
+  )
+}
+
+export default App
